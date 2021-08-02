9aa60bf426177f9d93e93913c883791efe6ad87a
Mantu <=_=>
2021-08-02 12:44:33 +0530

    Updates

---
+++ precache-manifest.46d73438fa870e01a2dbff9e10ae921c.js
@@ -0,0 +1,110 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "11bf5284f71fa3f5b0c571c07d0988ac",
+    "url": "/EndLess728.github.io/index.html"
+  },
+  {
+    "revision": "29d624950868128178a7",
+    "url": "/EndLess728.github.io/static/css/3.25727338.chunk.css"
+  },
+  {
+    "revision": "e21b63faba4441d79783",
+    "url": "/EndLess728.github.io/static/css/4.a8329f31.chunk.css"
+  },
+  {
+    "revision": "eb7deef71771ae0f09f3",
+    "url": "/EndLess728.github.io/static/css/main.aede627c.chunk.css"
+  },
+  {
+    "revision": "738af1f6a838dfaf2b17",
+    "url": "/EndLess728.github.io/static/js/2.3e361b8b.chunk.js"
+  },
+  {
+    "revision": "6e1d2efc12a1ac972ff1f942df7eb0bd",
+    "url": "/EndLess728.github.io/static/js/2.3e361b8b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "29d624950868128178a7",
+    "url": "/EndLess728.github.io/static/js/3.65793002.chunk.js"
+  },
+  {
+    "revision": "e21b63faba4441d79783",
+    "url": "/EndLess728.github.io/static/js/4.32f673d4.chunk.js"
+  },
+  {
+    "revision": "eb7deef71771ae0f09f3",
+    "url": "/EndLess728.github.io/static/js/main.643de047.chunk.js"
+  },
+  {
+    "revision": "1b38933ccb71bdc2713a",
+    "url": "/EndLess728.github.io/static/js/runtime-main.80c33a5b.js"
+  },
+  {
+    "revision": "21f233e19402cc4a66866a7f31191f0d",
+    "url": "/EndLess728.github.io/static/media/Agustina.21f233e1.woff"
+  },
+  {
+    "revision": "ee6539921d713482b8ccd4d0d23961bb",
+    "url": "/EndLess728.github.io/static/media/Montserrat-Regular.ee653992.ttf"
+  },
+  {
+    "revision": "c59720d63dc3595a12e1c7e641a1bfa5",
+    "url": "/EndLess728.github.io/static/media/airbnbLogo.c59720d6.png"
+  },
+  {
+    "revision": "47a7a717679b640ceb1b05d38c480a61",
+    "url": "/EndLess728.github.io/static/media/codeInLogo.47a7a717.webp"
+  },
+  {
+    "revision": "51cf7192327488da3d2a171280680c16",
+    "url": "/EndLess728.github.io/static/media/contactMailDark.51cf7192.svg"
+  },
+  {
+    "revision": "59389695208d7454c6607bed51ed4316",
+    "url": "/EndLess728.github.io/static/media/developerActivity.59389695.svg"
+  },
+  {
+    "revision": "8ddf76a14a2e3ad3ba62b46d49a75a74",
+    "url": "/EndLess728.github.io/static/media/facebookLogo.8ddf76a1.png"
+  },
+  {
+    "revision": "e79c639294c805688be731921368c8f8",
+    "url": "/EndLess728.github.io/static/media/googleAssistantLogo.e79c6392.webp"
+  },
+  {
+    "revision": "35ef5e915631be2c31ccdff55b53db29",
+    "url": "/EndLess728.github.io/static/media/harvardLogo.35ef5e91.png"
+  },
+  {
+    "revision": "52be31441c8cf8a8a16bd7a228d428bf",
+    "url": "/EndLess728.github.io/static/media/manOnTable.52be3144.svg"
+  },
+  {
+    "revision": "829ad0a1deef4d7295adfecdf12df2be",
+    "url": "/EndLess728.github.io/static/media/nextuLogo.829ad0a1.webp"
+  },
+  {
+    "revision": "ee62cb58630f29a6e6201b49d4c03728",
+    "url": "/EndLess728.github.io/static/media/pwaLogo.ee62cb58.webp"
+  },
+  {
+    "revision": "4b2938b9f5ea2f1ddbd45dccaff628bc",
+    "url": "/EndLess728.github.io/static/media/quoraLogo.4b2938b9.png"
+  },
+  {
+    "revision": "25018d29d5ab115bc51c3e9a2d92f90f",
+    "url": "/EndLess728.github.io/static/media/saayaHealthLogo.25018d29.webp"
+  },
+  {
+    "revision": "3440939881da8339d1597e8d77cdf850",
+    "url": "/EndLess728.github.io/static/media/skill.34409398.svg"
+  },
+  {
+    "revision": "3b758ef56d398145542e137daa28e392",
+    "url": "/EndLess728.github.io/static/media/stanfordLogo.3b758ef5.png"
+  },
+  {
+    "revision": "13780cdd0144cacdef25486e2c2503f8",
+    "url": "/EndLess728.github.io/static/media/talksCardBack.13780cdd.svg"
+  }
+]);
