1b43ea359fb077c28012414c083f9d1918e29c03
EndLess728 <EndLess728@users.noreply.github.com>
2024-03-18 12:23:54 +0000

    Deploying to master from  @ 4002a01bf179f7a7dae1ad46a9c3441f6337a174 🚀

---
+++ precache-manifest.5676018ce64875c1451a12b5f6ef0885.js
@@ -0,0 +1,126 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7dc1a01b5cc9bd5cf9aa6f53fc5b1660",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6e99a4b48a7ab438e3bd",
+    "url": "/static/css/3.df879eac.chunk.css"
+  },
+  {
+    "revision": "29921a8aeaa3ff663433",
+    "url": "/static/css/4.e91a4b36.chunk.css"
+  },
+  {
+    "revision": "2d2c8c98bfa385ff7d2f",
+    "url": "/static/css/main.2cca025a.chunk.css"
+  },
+  {
+    "revision": "04ee5f70205a3fbab1f1",
+    "url": "/static/js/2.04da1c16.chunk.js"
+  },
+  {
+    "revision": "408f8ade359af8695a328ed937291fdf",
+    "url": "/static/js/2.04da1c16.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6e99a4b48a7ab438e3bd",
+    "url": "/static/js/3.51f271ff.chunk.js"
+  },
+  {
+    "revision": "29921a8aeaa3ff663433",
+    "url": "/static/js/4.636f76aa.chunk.js"
+  },
+  {
+    "revision": "2d2c8c98bfa385ff7d2f",
+    "url": "/static/js/main.fbc8fc1d.chunk.js"
+  },
+  {
+    "revision": "9b3bb276d12053250992",
+    "url": "/static/js/runtime-main.a8e0c906.js"
+  },
+  {
+    "revision": "21f233e19402cc4a66866a7f31191f0d",
+    "url": "/static/media/Agustina.21f233e1.woff"
+  },
+  {
+    "revision": "ee6539921d713482b8ccd4d0d23961bb",
+    "url": "/static/media/Montserrat-Regular.ee653992.ttf"
+  },
+  {
+    "revision": "84397b1d7861362ba0420bf901b572d3",
+    "url": "/static/media/cbse.84397b1d.jpg"
+  },
+  {
+    "revision": "47a7a717679b640ceb1b05d38c480a61",
+    "url": "/static/media/codeInLogo.47a7a717.webp"
+  },
+  {
+    "revision": "51cf7192327488da3d2a171280680c16",
+    "url": "/static/media/contactMailDark.51cf7192.svg"
+  },
+  {
+    "revision": "59389695208d7454c6607bed51ed4316",
+    "url": "/static/media/developerActivity.59389695.svg"
+  },
+  {
+    "revision": "8c5fa704af698cecb3b4ca39b272ea5b",
+    "url": "/static/media/emove.8c5fa704.webp"
+  },
+  {
+    "revision": "227d1ae421b29908fa0702b51475b28d",
+    "url": "/static/media/eventickets.227d1ae4.webp"
+  },
+  {
+    "revision": "e79c639294c805688be731921368c8f8",
+    "url": "/static/media/googleAssistantLogo.e79c6392.webp"
+  },
+  {
+    "revision": "761b09372155818ca8db0bab3095f067",
+    "url": "/static/media/iSend.761b0937.webp"
+  },
+  {
+    "revision": "52be31441c8cf8a8a16bd7a228d428bf",
+    "url": "/static/media/manOnTable.52be3144.svg"
+  },
+  {
+    "revision": "7bc669175f7dc17da1326bf612d74d89",
+    "url": "/static/media/mealed.7bc66917.webp"
+  },
+  {
+    "revision": "60e91698b263fc74835c8b5593407f28",
+    "url": "/static/media/medsonwheels.60e91698.webp"
+  },
+  {
+    "revision": "ee62cb58630f29a6e6201b49d4c03728",
+    "url": "/static/media/pwaLogo.ee62cb58.webp"
+  },
+  {
+    "revision": "547ba276ad2b13c8995fdc45baef5b7e",
+    "url": "/static/media/ror.547ba276.webp"
+  },
+  {
+    "revision": "da6cb9f5f0626e26073633e72159a81b",
+    "url": "/static/media/school.da6cb9f5.png"
+  },
+  {
+    "revision": "3440939881da8339d1597e8d77cdf850",
+    "url": "/static/media/skill.34409398.svg"
+  },
+  {
+    "revision": "76d2d00c2a379200b1a785e5ca36e1ef",
+    "url": "/static/media/sukhmani.76d2d00c.jpeg"
+  },
+  {
+    "revision": "91398644969773df92c28ab0272fce2b",
+    "url": "/static/media/talkie.91398644.png"
+  },
+  {
+    "revision": "13780cdd0144cacdef25486e2c2503f8",
+    "url": "/static/media/talksCardBack.13780cdd.svg"
+  },
+  {
+    "revision": "66c28eed9d48fe241c1b0b275f157a2e",
+    "url": "/static/media/yogavivo.66c28eed.webp"
+  }
+]);
